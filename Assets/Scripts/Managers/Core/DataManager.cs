e2e3b7b970165b20081da976d830c32f22f5b7ef
SeongUHong <swhong369@gmail.com>
2024-07-26 21:48:57 +0900

    Update DataManager.cs

--- Assets/Scripts/Managers/Core/DataManager.cs
+++ Assets/Scripts/Managers/Core/DataManager.cs
@@ -9,18 +9,21 @@ public interface ILoader<key, value>
 
 public class DataManager
 {
+    const string ROOT_DIRECTORY = "Datas/";
+    const string STAT_DIRECTORY = ROOT_DIRECTORY + "Stats/";
+
     Loader LoadJson<Loader, key, value>(string path) where Loader : ILoader<key, value>
     {
-        TextAsset textAsset = Managers.Resource.Load<TextAsset>($"Datas/Stats/{path}");
+        TextAsset textAsset = Managers.Resource.Load<TextAsset>(path);
         return JsonUtility.FromJson<Loader>(textAsset.text);
     }
 
     public data.Stat GetStatByLevel(string path, int level)
     {
-        Dictionary <int, data.Stat> _statDict = LoadJson<data.StatLoader, int, data.Stat>(path).MakeDict();
+        Dictionary <int, data.Stat> _statDict = LoadJson<data.StatLoader, int, data.Stat>(STAT_DIRECTORY + path).MakeDict();
         if (_statDict == null || _statDict.Count == 0)
         {
-            Debug.Log($"Not Exist Json File : Datas/Stats/{path}");
+            Debug.Log($"Not Exist Json File : {STAT_DIRECTORY + path}");
             return null;
         }
 
