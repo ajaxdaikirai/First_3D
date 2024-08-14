1c80b4f724d56f6490969b2ee6dfd98b5bcd9615
SeongUHong <swhong369@gmail.com>
2024-08-14 21:56:05 +0900

    Update UIPopupGameover.cs

--- Assets/Scripts/UI/Popup/UIPopupGameover.cs
+++ Assets/Scripts/UI/Popup/UIPopupGameover.cs
@@ -1,7 +1,22 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class UIPopupGameover : UIPopup
 {
+    enum Buttons
+    {
+        MainSceneBtn,
+    }
+
+    public override void Init()
+    {
+        Bind<Button>(typeof(Buttons));
+        BindEvent(GetButton((int)Buttons.MainSceneBtn).gameObject, (PointerEventData data) => LoadMainScene(data));
+    }
+
+    // 메인씬으로 이동
+    public void LoadMainScene(PointerEventData data)
+    {
+        Managers.Scene.LoadScene(Define.Scenes.MainScene);
+    }
 }
